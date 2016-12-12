f5d77ee7783f774be421930a586beb6f4e03f2a0
Animesh Manglik <amanglik@whalerockindustries.com>
2016-12-12 11:31:58 -0800

    Swift Development: Multiplication Tables App

---
+++ README..md
@@ -0,0 +1,13 @@
+#Swift Development
+
+## Multiplication Table App
+
+Using concepts of
+
+- Tables
+- Slider
+
+Multiplication Table App
+
+- Multiples of number on slider
+- Show 50 multiples in table view
--- multiplicationTable/ViewController.swift
+++ multiplicationTable/ViewController.swift
@@ -8,8 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cellDefault")
+        
+        let multiple = (indexPath.row + 1) * Int(sliderValue.value)
+        
+        let outputText = "\(indexPath.row + 1) * \(Int(sliderValue.value)) = \(multiple)"
+        
+        cell.textLabel?.text = outputText
+        
+        return cell
+    }
+
+    
+    @IBOutlet weak var sliderValue: UISlider!
+
+    @IBAction func sliderPressed(_ sender: AnyObject) {
+            multiplicationTable.reloadData()
+    }
+
+    @IBOutlet weak var multiplicationTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
